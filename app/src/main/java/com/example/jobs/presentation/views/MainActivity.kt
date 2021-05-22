ca11b306c8426caadfa1b7cda4fd85f37363c84b
Hend123 <henda4544@gmail.com>
2021-05-22 10:40:06 +0200

    add first screen, all need libs and packages

---
+++ app/src/main/java/com/example/jobs/presentation/views/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.jobs.presentation.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.jobs.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
